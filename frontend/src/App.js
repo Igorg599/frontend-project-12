05b15d3d57c3645258d2b09f3a8fafd4515b8b19
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2022-11-07 11:17:07 +0500

    add form in login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,14 @@
-import Routes from "./features/routes"
+import { Route, Routes, BrowserRouter } from "react-router-dom"
+import { Home, Login, NotFound } from "./pages"
 
-const App = () => <Routes>11</Routes>
+const App = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="login" element={<Login />} />
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  </BrowserRouter>
+)
 
 export default App
