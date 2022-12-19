28ddd91d3ff132126cc2b2af71894245b77ebd58
Гордеев Игорь Юрьевич <IYGordeev@sberbank.ru>
2022-12-19 15:31:32 +0500

    add rollbar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import {
 } from "react-router-dom"
 import { Provider } from "react-redux"
 import { I18nextProvider } from "react-i18next"
+import { Provider as RollbarProvider, ErrorBoundary } from "@rollbar/react"
 import i18n from "i18n"
 import filterLeo from "leo-profanity"
 import useAuth from "hooks/useAuth"
@@ -21,30 +22,39 @@ const UseOutlet = () => {
   return auth.loggedIn ? <Outlet /> : <Navigate to="/login" />
 }
 
+const rollbarConfig = {
+  accessToken: process.env.REACT_APP_ROLLBAR_TOKEN,
+  environment: "production",
+}
+
 const App = () => {
   const ruLng = filterLeo.getDictionary("ru")
   filterLeo.add(ruLng)
 
   return (
-    <Provider store={store}>
-      <I18nextProvider i18n={i18n}>
-        <SocketContext.Provider value={socket}>
-          <AuthProvider>
-            <SideBar />
-            <BrowserRouter>
-              <Routes>
-                <Route path="/" element={<UseOutlet />}>
-                  <Route path="" element={<Home />} />
-                </Route>
-                <Route path="/login" element={<Login />} />
-                <Route path="/signup" element={<Registration />} />
-                <Route path="*" element={<NotFound />} />
-              </Routes>
-            </BrowserRouter>
-          </AuthProvider>
-        </SocketContext.Provider>
-      </I18nextProvider>
-    </Provider>
+    <RollbarProvider config={rollbarConfig}>
+      <ErrorBoundary>
+        <Provider store={store}>
+          <I18nextProvider i18n={i18n}>
+            <SocketContext.Provider value={socket}>
+              <AuthProvider>
+                <SideBar />
+                <BrowserRouter>
+                  <Routes>
+                    <Route path="/" element={<UseOutlet />}>
+                      <Route path="" element={<Home />} />
+                    </Route>
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/signup" element={<Registration />} />
+                    <Route path="*" element={<NotFound />} />
+                  </Routes>
+                </BrowserRouter>
+              </AuthProvider>
+            </SocketContext.Provider>
+          </I18nextProvider>
+        </Provider>
+      </ErrorBoundary>
+    </RollbarProvider>
   )
 }
 
